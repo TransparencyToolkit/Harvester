2476640fa01440f4e5f23d244f9e333c129ae7c6
shidash <shidash@shidash.com>
2015-12-03 22:26:43 -0500

    Added backend for processing multiple input queries

--- app/controllers/datasets_controller.rb
+++ app/controllers/datasets_controller.rb
@@ -15,10 +15,8 @@ class DatasetsController < ApplicationController
   def create
     @dataset = Dataset.new(dataset_params)
     @dataset.save
-    @query = gen_query_url(params)
-    @results = JSON.parse(Curl.get(@query).body_str)
-    save_data(@results, @dataset, params)
-   
+    loop_and_run(params, @dataset)
+  
     respond_to do |format|
       if @dataset.save
         format.html { redirect_to @dataset, notice: 'Dataset was successfully created.' }
@@ -30,29 +28,46 @@ class DatasetsController < ApplicationController
     end
   end
 
+  # Loop through all terms and run
+  def loop_and_run(params, dataset)
+    params[:input_query_fields].each do |key, value|
+      query = gen_query_url(value, params)
+      results = JSON.parse(Curl.get(query).body_str)
+      save_data(results, dataset, params, val_string(value))
+    end
+  end
+
   # Put all items for dataset in JSON
   def show
     @print_data = gen_print_data(@dataset)
   end
 
   private
-
-  def make_datasetdir(dataset)
-    unless File.directory?("~/Data/KG/test_dir")
-      Dir.mkdir("~/Data/KG/test_dir")
+  
+  # Generate value string name for files
+  def val_string(value)
+    value_str = ""
+    value.each do |k, v|
+      value_str += v.gsub(" ", "_").gsub("/", "-").gsub(":", "")+"_"
     end
+    return value_str
   end
 
-  def save_data_files(params, print_data)
-    dataset_name = params["dataset"]["name"]
-    dirname = ENV['HOME']+"/Data/KG/"
-    filename = dirname+dataset_name.gsub(" ", "_").gsub("/", "-")+".json"
+  # Save folders of data files
+  def save_data_files(params, print_data, out_file_name)
+    # Create results directory with name based on dataset
+    results_dir = ENV['HOME']+"/Data/KG/"+params["dataset"]["name"].gsub(" ", "_").gsub("/", "-")+"_"+params["source"]+"/"
+    unless File.directory?(results_dir)
+      Dir.mkdir(results_dir)
+    end
+    
+    # Set output filename based on output and timestamp
+    filename = results_dir+out_file_name+Time.now.to_s.split(" ")[0].gsub("-", "")+".json"
     File.write(filename, print_data)
-    # TODO: Later add files per search term and split by data source
   end
 
   # Save all data
-  def save_data(results, dataset, params)
+  def save_data(results, dataset, params, out_file_name)
     results.each do |dataitem|
       # Create item for appropriate model
       classname = get_item_classname(params["source"])
@@ -64,8 +79,7 @@ class DatasetsController < ApplicationController
       item.dataset = dataset
     end
 
-    print_data = gen_print_data(dataset)
-    save_data_files(params, print_data)
+    save_data_files(params, JSON.pretty_generate(results), out_file_name)
   end
 
   # Generate parameters hash
@@ -85,6 +99,7 @@ class DatasetsController < ApplicationController
     gen_class_name(name)
   end
 
+  # Generate JSON with all data to print on show view
   def gen_print_data(dataset)
     result_data = dataset.dataitems.inject([]) do |all_items, item|
       all_items.push(item.as_json.first[1])
@@ -93,7 +108,7 @@ class DatasetsController < ApplicationController
   end
 
   # Generate URL with params and crawler info
-  def gen_query_url(params)
+  def gen_query_url(query, params)
     @input_params = JSON.parse(Curl.get("http://0.0.0.0:9506/get_crawler_info?crawler="+params["source"]).body_str)["input_params"]
     
     # Gen url base
@@ -102,7 +117,7 @@ class DatasetsController < ApplicationController
     
     # Add all params for dataset
     @input_params.each do |param, type|
-      url += "&"+param+"="+Base64.encode64(params[param]).strip
+      url += "&"+param+"="+Base64.encode64(query[param]).strip
     end
     
     return url
@@ -115,6 +130,6 @@ class DatasetsController < ApplicationController
 
   # Never trust parameters from the scary internet, only allow the white list through.
   def dataset_params
-    params.require(:dataset).permit(:name)
+    params.require(:dataset).permit(:name, :input_query_fields)
   end
 end
--- app/models/dataset.rb
+++ app/models/dataset.rb
@@ -3,11 +3,4 @@ class Dataset
   property :name, type: String
   property :input_query_fields
   has_many :in, :dataitems, origin: :dataset
-
-  # And add input field- for JSON of queries
-  # Group and save in input
-  # Update query manager
-  
-  # Add dynamic # of inputs on form
-  # Add function to run queries one after the other and parse correctly and save
 end

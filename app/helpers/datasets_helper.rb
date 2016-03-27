f0d45fd8aac45ce8d86e8c36474d517a0df8151d
shidash <shidash@shidash.com>
2016-03-27 18:06:33 -0400

    Added dataitem count helper methods

--- app/helpers/datasets_helper.rb
+++ app/helpers/datasets_helper.rb
@@ -1,6 +1,12 @@
 module DatasetsHelper
+  # Get count of all results
+  def get_overall_count
+    #TODO: Will need to modify this to work with domains!
+    return Dataitem.all.length
+  end
 
-  def get_count(dataset)
-    
+  # Get number of results for single dataset
+  def get_count_per_dataset(dataset)
+    return dataset.dataitems.length
   end
 end

1267e46d36839088e77acc0323a3d32f88a5fc8f
shidash <shidash@shidash.com>
2017-02-22 22:35:46 -0500

    Improved performance of result count

--- app/helpers/datasets_helper.rb
+++ app/helpers/datasets_helper.rb
@@ -2,19 +2,12 @@ module DatasetsHelper
   # Get count of all results
   def get_overall_count
     #TODO: Will need to modify this to work with domains!
-    return count_unique(Dataitem.all)
+    return Dataitem.distinct(:matching_id).length
   end
 
   # Get number of results for single dataset
   def get_count_per_dataset(dataset)
-    return count_unique(dataset.dataitems)
-  end
-
-  # Get number of items with unique ID in set
-  def count_unique(items)
-    id_arr = Array.new
-    items.each{|i| id_arr.push(i["matching_id"]) if i["matching_id"]}
-    return id_arr.uniq.length
+    return dataset.dataitems.distinct(:matching_id).length
   end
 
   # Generate JSON with all data to print on show view
